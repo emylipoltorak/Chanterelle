b7098520cbcba2c0c2d3e7ca0a12058e8819e12f
Emyli Poltorak <emylipoltorak@Emylis-MacBook-Pro-1126.local>
2018-01-23 16:14:29 -0800

    blank project set up

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+const BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+    context: __dirname,
+    entry: './assets/js/index',
+
+    output: {
+        //where you want your compiled bundle to be stored
+        path: path.resolve('./assets/bundles/'),
+        //naming convention webpack should use for your files
+        filename: '[name]-[hash].js'
+    },
+
+    plugins: [
+        //tells webpack where to store data about your bundles.
+        new BundleTracker({filename: './webpack-stats.json'})
+    ],
+
+    module: {
+      rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      }
+      ]
+
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
