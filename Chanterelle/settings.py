c5787e35881d77f21dc030e641bd6485df47741b
Emyli Poltorak <emylipoltorak@gmail.com>
2018-02-23 12:17:07 -0800

    static root

--- Chanterelle/settings.py
+++ Chanterelle/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.joing(BASE_DIR, 'api', 'static')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
     os.path.join(BASE_DIR, 'api', 'static'),
