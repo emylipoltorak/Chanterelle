c6cf62c7fbc7a22dc012ce22d631131e01336022
Emyli Poltorak <emylipoltorak@gmail.com>
2018-03-07 18:39:33 -0800

    test

--- Chanterelle/settings.py
+++ Chanterelle/settings.py
@@ -20,11 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if not DEBUG:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-else:
-    from .local_settings import LOCAL_KEY
-    SECRET_KEY = LOCAL_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -33,6 +28,11 @@ ALLOWED_HOSTS = [
     '*'
 ]
 
+if not DEBUG:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+else:
+    from .local_settings import LOCAL_KEY
+    SECRET_KEY = LOCAL_KEY
 
 # Application definition
 
